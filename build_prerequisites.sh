4e0c7788ea74ad6accf5ac1fc9fcc920022d26cd
VfxPro99 <vfxpro99@gmail.com>
2016-08-04 14:22:30 -0700

    Stop the build if any prerequiste fails to build.

--- build_prerequisites.sh
+++ build_prerequisites.sh
@@ -13,18 +13,83 @@ if [ $rc != 0 ]; then
 fi
 
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/double-conversion.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/glew.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 #source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/numpy.sh
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenColorIO.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenEXR.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenSubdiv.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/jpeg.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/png.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/ptex.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/tiff.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/tbb.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/hdf5.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/alembic.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenImageIO.sh
+rc=$?
+if [ $rc != 0 ]; then
+  exit $rc
+fi
+
 #source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/qt4.sh
 #source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/pyside.sh
