ef49803d1a3dd1d53cf3256ae426ff73b2b2331b
VfxPro99 <vfxpro99@gmail.com>
2016-08-06 13:02:35 -0700

    Fix rpathing in OpenEXR and OpenSubdiv

--- prerequisites-macOS/OpenEXR.sh
+++ prerequisites-macOS/OpenEXR.sh
@@ -27,42 +27,32 @@ fi
 if [ -f build/ilmbase ]; then
   rm -rf build/ilmbase
 fi
-
 mkdir -p build/ilmbase
 
 if [ -f build/openexr ]; then
   rm -rf build/openexr
 fi
+mkdir -p build/openexr
 
 cd build/ilmbase
 
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local ${ROOT}/prereq/openexr/IlmBase
+cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local \
+      -DCMAKE_INSTALL_NAME_DIR=@rpath \
+      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON \
+      -DBUILD_WITH_INSTALL_RPATH=1 \
+      ${ROOT}/prereq/openexr/IlmBase
 make -j 4
 make install
 
 cd ../openexr
 
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local -DILMBASE_PACKAGE_PREFIX=${ROOT}/local ${ROOT}/prereq/openexr/OpenEXR
+cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local \
+      -DCMAKE_INSTALL_NAME_DIR=@rpath \
+      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON \
+      -DBUILD_WITH_INSTALL_RPATH=1 \
+      -DILMBASE_PACKAGE_PREFIX=${ROOT}/local \
+      ${ROOT}/prereq/openexr/OpenEXR
 make -j 4
 make install
 
 cd ${ROOT}
-
-if [ ! -f "${ROOT}/local/lib/libIex.dylib" ]; then
-  ln -s -f ${ROOT}/local/libIex-2_2.12.0.0.dylib ${ROOT}/local/lib/libIex.dylib
-fi
-if [ ! -f "${ROOT}/local/lib/libIexMath.dylib" ]; then
-  ln -s -f ${ROOT}/local/lib/libIexMath-2_2.12.0.0.dylib ${ROOT}/local/lib/libIexMath.dylib
-fi
-if [ ! -f "${ROOT}/local/lib/libIlmImf.dylib" ]; then
-  ln -s -f ${ROOT}/local/lib/libIlmImf-2_2.12.0.0.dylib ${ROOT}/local/lib/libIlmImf.dylib
-fi
-if [ ! -f "${ROOT}/local/lib/libIlmImfUtil.dylib" ]; then
-  ln -s -f ${ROOT}/local/lib/libIlmImfUtil-2_2.12.0.0.dylib ${ROOT}/local/lib/libIlmImfUtil.dylib
-fi
-if [ ! -f "${ROOT}/local/lib/libIlmThread.dylib" ]; then
-  ln -s -f ${ROOT}/local/lib/libIlmThread-2_2.12.0.0.dylib ${ROOT}/local/lib/libIlmThread.dylib
-fi
-if [ ! -f "${ROOT}/local/lib/libImath.dylib" ]; then
-  ln -s -f ${ROOT}/local/lib/libImath-2_2.12.0.0.dylib ${ROOT}/local/lib/libImath.dylib
-fi
--- prerequisites-macOS/OpenSubdiv.sh
+++ prerequisites-macOS/OpenSubdiv.sh
@@ -1,64 +1,81 @@
 #! /bin/sh
 
-if [ ! -f local/lib/libosdCPU.a ]; then
+if [ -f local/lib/libosdCPU.a ]; then
+  exit 0
+fi
 
-  if [ ! -f prereq ]; then
-    mkdir -p prereq
-  fi
-  if [ ! -f local/lib ]; then
-    mkdir -p local/lib
-  fi
-  if [ ! -f local/bin ]; then
-    mkdir -p local/bin
-  fi
-  if [ ! -f local/include ]; then
-    mkdir -p local/include
-  fi
-  if [ ! -f local/include/opensubdiv/far ]; then
-    mkdir -p local/include/opensubdiv/far
-  fi
-  if [ ! -f local/include/opensubdiv/hbr ]; then
-    mkdir -p local/include/opensubdiv/hbr
-  fi
-  if [ ! -f local/include/opensubdiv/osd ]; then
-    mkdir -p local/include/opensubdiv/osd
-  fi
-  if [ ! -f local/include/opensubdiv/sdc ]; then
-    mkdir -p local/include/opensubdiv/sdc
-  fi
-  if [ ! -f local/include/opensubdiv/vtr ]; then
-    mkdir -p local/include/opensubdiv/vtr
-  fi
+if [ ! -f prereq ]; then
+  mkdir -p prereq
+fi
+if [ ! -f local/lib ]; then
+  mkdir -p local/lib
+fi
+if [ ! -f local/bin ]; then
+  mkdir -p local/bin
+fi
+if [ ! -f local/include ]; then
+  mkdir -p local/include
+fi
+if [ ! -f local/include/opensubdiv/far ]; then
+  mkdir -p local/include/opensubdiv/far
+fi
+if [ ! -f local/include/opensubdiv/hbr ]; then
+  mkdir -p local/include/opensubdiv/hbr
+fi
+if [ ! -f local/include/opensubdiv/osd ]; then
+  mkdir -p local/include/opensubdiv/osd
+fi
+if [ ! -f local/include/opensubdiv/sdc ]; then
+  mkdir -p local/include/opensubdiv/sdc
+fi
+if [ ! -f local/include/opensubdiv/vtr ]; then
+  mkdir -p local/include/opensubdiv/vtr
+fi
 
-  SCRIPTDIR=`dirname $0`
+SCRIPTDIR=`dirname $0`
 
-  ROOT=$(pwd)
-  cd prereq
-  if [ ! -f OpenSubdiv/.git/config ]; then
-    git clone git://github.com/PixarAnimationStudios/OpenSubdiv.git
-  else
-    cd OpenSubdiv; git pull; cd ..
-  fi
+ROOT=$(pwd)
+cd prereq
+if [ ! -f OpenSubdiv/.git/config ]; then
+  git clone git://github.com/PixarAnimationStudios/OpenSubdiv.git
+else
+  cd OpenSubdiv; git pull; cd ..
+fi
 
-  cd OpenSubdiv
-  cp ./../../${SCRIPTDIR}/FindPTex.cmake cmake/FindPtex.cmake
+if [ -f build/OpenSubdiv ]; then
+  rm -rf build/OpenSubdiv
+fi
+mkdir -p build/OpenSubdiv
 
-  mkdir build;cd build
-  # temporary: suppress ptex until OSD is updated on github
-  cmake -DNOPTEX=1 -DCMAKE_INSTALL_PREFIX=${ROOT}/local -DGLEW_LOCATION=${ROOT}/local ..
-  make -j 4
-  cp lib/*.a ${ROOT}/local/lib
-  cp lib/*.dylib ${ROOT}/local/lib
-  cd ..
-  cp opensubdiv/*.h ${ROOT}/local/include/opensubdiv
-  cp opensubdiv/far/*.h ${ROOT}/local/include/opensubdiv/far
-  cp opensubdiv/hbr/*.h ${ROOT}/local/include/opensubdiv/hbr
-  cp opensubdiv/osd/*.h ${ROOT}/local/include/opensubdiv/osd
-  cp opensubdiv/sdc/*.h ${ROOT}/local/include/opensubdiv/sdc
-  cp opensubdiv/vtr/*.h ${ROOT}/local/include/opensubdiv/vtr
-  cd ${ROOT}
+cd OpenSubdiv
+cp ./../../${SCRIPTDIR}/FindPTex.cmake cmake/FindPtex.cmake
+cd ..
 
-  if [ ! -d "${ROOT}/local/include/opensubdiv3" ]; then
-    cp -R ${ROOT}/local/include/opensubdiv ${ROOT}/local/include/opensubdiv3
-  fi
+cd build/OpenSubdiv
+
+cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local \
+      -DGLEW_LOCATION=${ROOT}/local \
+      -DCMAKE_INSTALL_NAME_DIR=@rpath \
+      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON \
+      -DBUILD_WITH_INSTALL_RPATH=1 \
+      ../../OpenSubdiv
+
+make -j 4
+make install
+
+cd ${ROOT}
+
+#cp lib/*.a ${ROOT}/local/lib
+#cp lib/*.dylib ${ROOT}/local/lib
+#cd ..
+#cp opensubdiv/*.h ${ROOT}/local/include/opensubdiv
+#cp opensubdiv/far/*.h ${ROOT}/local/include/opensubdiv/far
+#cp opensubdiv/hbr/*.h ${ROOT}/local/include/opensubdiv/hbr
+#cp opensubdiv/osd/*.h ${ROOT}/local/include/opensubdiv/osd
+#cp opensubdiv/sdc/*.h ${ROOT}/local/include/opensubdiv/sdc
+#cp opensubdiv/vtr/*.h ${ROOT}/local/include/opensubdiv/vtr
+#cd ${ROOT}
+
+if [ ! -d "${ROOT}/local/include/opensubdiv3" ]; then
+  cp -R ${ROOT}/local/include/opensubdiv ${ROOT}/local/include/opensubdiv3
 fi
