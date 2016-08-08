8e58870598565b2f13f81ed372c18b090dd6cd83
VfxPro99 <vfxpro99@gmail.com>
2016-08-08 16:43:37 -0700

    Qt4 now builds

--- prerequisites-macOS/pyside.sh
+++ prerequisites-macOS/pyside.sh
@@ -1,17 +1,23 @@
 #! /bin/sh
 
-SCRIPTDIR="$(dirname "$(which "$0")")"
+ROOT=$(pwd)
+PREREQ_SCRIPTDIR=`dirname $0`
+
+QT_BUILD_ROOT=/usr/local
 
 # shiboken
 
-ROOT=$(pwd)
 cd prereq
 curl -L -o shiboken.tgz http://download.qt.io/official_releases/pyside/shiboken-1.2.2.tar.bz2
 gunzip shiboken.tgz
 tar -xvf shiboken.tar
-cd shiboken-1.2.2
-mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local -DALTERNATIVE_QT_INCLUDE_DIR=${ROOT}/local ..
+
+mkdir -p build/shiboken
+cd build/shiboken
+
+cmake -DCMAKE_INSTALL_PREFIX=${QT_BUILD_ROOT} -DALTERNATIVE_QT_INCLUDE_DIR=${QT_BUILD_ROOT} \
+      ../../shiboken-1.2.2
+
 make -j 4; make install
 cd ${ROOT}/prereq
 
@@ -21,9 +27,15 @@ curl -L -o pyside-qt.tgz http://download.qt.io/official_releases/pyside/pyside-q
 gunzip pyside-qt.tgz
 tar -xvf pyside-qt.tar
 cd pyside-qt4.8+1.2.2
-cp /Users/dp/Projects/usd_160409/xcbuild/usd_prereq_osx/pyside.cmake CMakeLists.txt
-mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local -DALTERNATIVE_QT_INCLUDE_DIR=${ROOT}/local -DBUILD_TESTS=False -DENABLE_ICECC=0 ..
+cp ${ROOT}/${PREREQ_SCRIPTDIR}/pyside.cmake CMakeLists.txt
+cd ..
+mkdir -p build/pyside
+cd build/psyide
+
+cmake -DCMAKE_INSTALL_PREFIX=${QT_BUILD_ROOT} -DALTERNATIVE_QT_INCLUDE_DIR=${QT_BUILD_ROOT} \
+      -DBUILD_TESTS=False -DENABLE_ICECC=0
+      ../../pyside-qt4.8+1.2.2
+
 make -j 4; make install
 cd ${ROOT}/prereq
 
@@ -32,9 +44,13 @@ cd ${ROOT}/prereq
 curl -L -o pyside-tools.tgz https://github.com/PySide/Tools/archive/0.2.15.tar.gz
 gunzip pyside-tools.tgz
 tar -xvf pyside-tools.tar
-cd Tools-0.2.15
-mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT}/local -DALTERNATIVE_QT_INCLUDE_DIR=${ROOT}/local ..
+
+mkdir -p build/pyside-tools
+cd build/pyside-tools
+
+cmake -DCMAKE_INSTALL_PREFIX=${QT_BUILD_ROOT} -DALTERNATIVE_QT_INCLUDE_DIR=${QT_BUILD_ROOT} \
+      ../../Tools-0.2.15
+
 make -j 4; make install
 
 cd ${ROOT}
--- prerequisites-macOS/qt4.sh
+++ prerequisites-macOS/qt4.sh
@@ -1,32 +1,80 @@
 
 #! /bin/sh
+if [ -f /usr/local/lib/libQt4Core.dylib ]; then
+  exit 0
+fi
 
 if [ ! -f prereq ]; then
   mkdir -p prereq
 fi
-if [ ! -f local/lib ]; then
-  mkdir -p local/lib
-fi
-if [ ! -f local/bin ]; then
-  mkdir -p local/bin
-fi
-if [ ! -f local/include ]; then
-  mkdir -p local/include
-fi
 
 ROOT=$(pwd)
+PREREQ_SCRIPTDIR=`dirname $0`
+QT_DIR=qt-everywhere-opensource-src-4.8.7
+
 cd prereq
-#curl -L -o qt-everywhere.tgz http://download.qt.io/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz
-#gunzip qt-everywhere.tgz
-#tar -xvf qt-everywhere.tar
-cd qt-everywhere-opensource-src-4.8.7
+
+if [ ! -f ${QT_DIR}/README ]; then
+  curl -L -o qt-everywhere.tgz http://download.qt.io/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo Could not retrieve qt tarball
+    exit $rc
+  fi
+  gunzip qt-everywhere.tgz
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo Could not unzip qt tarball
+    exit $rc
+  fi
+  tar -xvf qt-everywhere.tar
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo Could not untar qt tarball
+    exit $rc
+  fi
+fi
+
 #configure hacked to force generation of STL support required for pyside. Detection is busted on el capitan+.
-cp /Users/dp/Projects/usd_160409/xcbuild/usd_prereq_osx/configure.qt4 configure
-./configure -prefix ${ROOT}/local -opensource -confirm-license -no-qt3support -shared -no-multimedia -stl \
- -nomake examples -nomake demos -nomake docs -nomake translations -no-phonon -silent -no-framework \
- -arch x86_64 -platform unsupported/macx-clang -v -continue
+cp ${ROOT}/${PREREQ_SCRIPTDIR}/configure.qt4 ${ROOT}/prereq/${QT_DIR}/configure
+
+mkdir build/qt-everywhere
+cd build/qt-everywhere
+
+# learn more:
+# http://doc.qt.io/qt-5/configure-options.html
+
+sudo ${ROOT}/prereq/${QT_DIR}/configure -prefix /usr/local \
+            -opensource -confirm-license -silent \
+            -no-qt3support \
+            -nomake examples -nomake demos -nomake docs -nomake translations \
+            -no-phonon -no-multimedia \
+            -shared -stl -no-framework \
+            -arch x86_64 -platform unsupported/macx-clang \
+            -v -continue
+
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo Could not configure qt
+  exit $rc
+fi
+
 # update file for el capitan compatibility
-cp /Users/dp/Projects/usd_160409/xcbuild/usd_prereq_osx/qpaintengine_mac.cpp src/gui/painting/qpaintengine_mac.cpp
-make -j 4; make install
-sudo cp bin/qmake /usr/local/bin/
+cp ${ROOT}/${PREREQ_SCRIPTDIR}/qpaintengine_mac.cpp ${ROOT}/prereq/${QT_DIR}/src/gui/painting/qpaintengine_mac.cpp
+sudo make -j 4
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo Could not make qt
+  exit $rc
+fi
+
+sudo make install
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo Could not install qt
+  exit $rc
+fi
+
+#sudo cp bin/qmake /usr/local/bin/
+
 cd ${ROOT}
