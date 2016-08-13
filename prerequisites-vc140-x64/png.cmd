0984758e25e2dba66bcfdcf13a8a11ffaec5864d
VfxPro99 <vfxpro99@gmail.com>
2016-08-13 12:05:43 -0700

    Png Tiff windows

--- prerequisites-vc140-x64/png.cmd
+++ prerequisites-vc140-x64/png.cmd
@@ -1,13 +1,25 @@
 SET current=%cd%
 cd prereq
+
+if not exist "build\libpng" ^
+mkdir build\libpng
+
+if not exist "libpng\CMakeLists.txt" ^
 git clone git://github.com/glennrp/libpng.git
+
 cd libpng
-mkdir build_win
-cd build_win
+git pull
+cd ..
+
+cd build\libpng
+
 cmake -G "Visual Studio 14 2015 Win64"^
       -DCMAKE_PREFIX_PATH="%current%\local"^
-      -DCMAKE_INSTALL_PREFIX="%current%\local" ..
-msbuild libpng.sln /t:Build /p:Configuration=Release /p:Platform=x64
+      -DCMAKE_INSTALL_PREFIX="%current%\local" ..\..\libpng
+
+cmake --build . --target install --config Release
+
+rem msbuild libpng.sln /t:Build /p:Configuration=Release /p:Platform=x64
 cd %current%
-xcopy .\prereq\libpng\build_win\Release\libpng*.* .\local\lib\ /s /y
-xcopy .\prereq\libpng\png*.h .\local\include /s /y
+rem xcopy .\prereq\libpng\build_win\Release\libpng*.* .\local\lib\ /s /y
+rem xcopy .\prereq\libpng\png*.h .\local\include /s /y
--- prerequisites-vc140-x64/tiff.cmd
+++ prerequisites-vc140-x64/tiff.cmd
@@ -1,13 +1,25 @@
 SET current=%cd%
 cd prereq
+
+if not exist "build\libtiff" ^
 git clone https://github.com/vadz/libtiff
+
 cd libtiff
-mkdir build_win
-cd build_win
+git pull
+cd ..
+
+if not exist "build\libtiff" ^
+mkdir build\libtiff
+
+cd build\libtiff
+
 cmake -G "Visual Studio 14 2015 Win64"^
       -DCMAKE_PREFIX_PATH="%current%\local"^
-      -DCMAKE_INSTALL_PREFIX="%current%\local" ..
-msbuild tiff.sln /t:Build /p:Configuration=Release /p:Platform=x64
+      -DCMAKE_INSTALL_PREFIX="%current%\local" ..\..\libtiff
+
+cmake --build . --target install --config Release
+
+rem msbuild tiff.sln /t:Build /p:Configuration=Release /p:Platform=x64
 cd %current%
-xcopy .\prereq\libtiff\build_win\libtiff\Release\tiff.* .\local\lib\ /s /y
-xcopy .\prereq\libtiff\libtiff\tiff*.h .\local\include\ /s /y
+rem xcopy .\prereq\libtiff\build_win\libtiff\Release\tiff.* .\local\lib\ /s /y
+rem xcopy .\prereq\libtiff\libtiff\tiff*.h .\local\include\ /s /y
