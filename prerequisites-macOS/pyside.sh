3a3f423e5002676a0fb5515007229c88726349c1
VfxPro99 <vfxpro99@gmail.com>
2016-08-30 16:25:45 -0700

    Repair more of the PySide Things

--- prerequisites-macOS/pyside.sh
+++ prerequisites-macOS/pyside.sh
@@ -6,14 +6,34 @@ sudo pip install -U PySide
 
 # repair PySide installation
 
+sudo install_name_tool -change @rpath/libshiboken-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/libshiboken-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/libpyside-python2.7.1.2.dylib
+
+
 sudo install_name_tool -change @rpath/libpyside-python2.7.1.2.dylib \
                                /Library/Python/2.7/site-packages/PySide/libpyside-python2.7.1.2.dylib \
                                /Library/Python/2.7/site-packages/PySide/QtCore.so 
 
+sudo install_name_tool -change @rpath/libpyside-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/libpyside-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/QtGui.so 
+
+sudo install_name_tool -change @rpath/libpyside-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/libpyside-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/QtOpenGL.so 
+                               
+
 sudo install_name_tool -change @rpath/libshiboken-python2.7.1.2.dylib \
                                /Library/Python/2.7/site-packages/PySide/libshiboken-python2.7.1.2.dylib \
                                /Library/Python/2.7/site-packages/PySide/QtCore.so 
 
 sudo install_name_tool -change @rpath/libshiboken-python2.7.1.2.dylib \
                                /Library/Python/2.7/site-packages/PySide/libshiboken-python2.7.1.2.dylib \
-                               /Library/Python/2.7/site-packages/PySide/libpyside-python2.7.1.2.dylib
+                               /Library/Python/2.7/site-packages/PySide/QtGui.so 
+
+sudo install_name_tool -change @rpath/libshiboken-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/libshiboken-python2.7.1.2.dylib \
+                               /Library/Python/2.7/site-packages/PySide/QtOpenGL.so 
+
+
