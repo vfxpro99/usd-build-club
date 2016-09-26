2374db6102c85355a2fc61be6eb2a0deed70afd6
VfxPro99 <vfxpro99@gmail.com>
2016-09-25 21:57:12 -0700

    Qt configuration

--- configure.cmd
+++ configure.cmd
@@ -16,9 +16,14 @@ cmake ..\..\..\..\USD_win ^
       -DOIIO_LOCATION="%builddir%" ^
       -DOPENEXR_ROOT_DIR="%builddir%" ^
       -DOPENSUBDIV_ROOT_DIR="%builddir%" ^
-      -DQT_ROOT_DIR="%builddir%" ^
       -DPTEX_LOCATION="%builddir%" ^
       -DTBB_ROOT_DIR="%builddir%" ^
+      -DQt5Core_DIR="C:\qt\5.7\msvc2015_64\lib\cmake\Qt5Core" ^
+      -DQt5Gui_DIR="C:\qt\5.7\msvc2015_64\lib\cmake\Qt5Gui" ^
+      -DQt5OpenGL_DIR="C:\qt\5.7\msvc2015_64\lib\cmake\Qt5OpenGL" ^
+      -DQt5Network_DIR="C:\qt\5.7\msvc2015_64\lib\cmake\Qt5Network" ^
+      -DQt5Xml_DIR="C:\qt\5.7\msvc2015_64\lib\cmake\Qt5Xml" ^
+      -DPYSIDE_BIN_DIR="C:\Python27\Scripts" ^
       -DBoost_INCLUDE_DIR="%builddir%"/include -DBoost_LIBRARY_DIR="%builddir%"/lib ^
       -DPXR_INSTALL_LOCATION="%builddir%" ^
       -G "Visual Studio 14 2015 Win64"
