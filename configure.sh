fa147cf1829a8a53a40168a68f1e63df02d51018
VfxPro99 <vfxpro99@gmail.com>
2016-09-30 13:20:03 -0700

    Update for macOS build for Maya 2017

--- configure.sh
+++ configure.sh
@@ -10,6 +10,32 @@ echo "build from USD sources at ${SOURCEDIR}/pxr"
 echo "local dir for prerequisites is ${BUILDDIR}"
 
 if [ $# > 1 ]; then
+  if [[ "$1" = "Maya" ]]; then
+    echo "Configuring for Xcode & Maya"
+    cmake ${SOURCEDIR} \
+      -DPXR_BUILD_MAYA_PLUGIN=1 \
+      -DMAYA_LOCATION=/Applications/Autodesk/maya2017/Maya.app/Contents \
+      -DPXR_tbb_LIBRARY=/Applications/Autodesk/maya2017/Maya.app/Contents/MacOS/libtbb.dylib \
+      -DPXR_MALLOC_LIBRARY:path=/Applications/Autodesk/maya2017/Maya.app/Contents/MacOS/libtbbmalloc.dylib \
+      -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents//Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk/ \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+      -DPXR_INSTALL_LOCATION=/Users/Shared/Autodesk/maya/2017/plug-ins/pxrUsd \
+      -DCMAKE_INSTALL_PREFIX=${BUILDDIR} \
+      -DCMAKE_PREFIX_PATH=${BUILDDIR} \
+      -DALEMBIC_DIR=${BUILDDIR} \
+      -DDOUBLE_CONVERSION_DIR=${BUILDDIR} \
+      -DGLEW_LOCATION=${BUILDDIR} \
+      -DOIIO_LOCATION=${BUILDDIR} \
+      -DOPENEXR_ROOT_DIR=${BUILDDIR} \
+      -DOPENSUBDIV_ROOT_DIR=${BUILDDIR} \
+      -DQT_USE_FRAMEWORKS=0 \
+      -DQT_ROOT_DIR=${BUILDDIR} \
+      -DPTEX_LOCATION=${BUILDDIR} \
+      -DTBB_ROOT_DIR=${BUILDDIR} \
+      -DBoost_INCLUDE_DIR=${BUILDDIR}/include -DBoost_LIBRARY_DIR=${BUILDDIR}/lib \
+      -G Xcode
+    exit 
+  fi
   if [[ "$1" = "Xcode" ]]; then
     echo "Configuring for Xcode"
     cmake ${SOURCEDIR} \
@@ -27,21 +53,21 @@ if [ $# > 1 ]; then
       -DTBB_ROOT_DIR=${BUILDDIR} \
       -DBoost_INCLUDE_DIR=${BUILDDIR}/include -DBoost_LIBRARY_DIR=${BUILDDIR}/lib \
       -G Xcode
-  else
-    echo "Configuring for make"
-    cmake ${SOURCEDIR} \
-      -DCMAKE_INSTALL_PREFIX=${BUILDDIR} \
-      -DCMAKE_PREFIX_PATH=${BUILDDIR} \
-      -DALEMBIC_DIR=${BUILDDIR} \
-      -DDOUBLE_CONVERSION_DIR=${BUILDDIR} \
-      -DGLEW_LOCATION=${BUILDDIR} \
-      -DOIIO_LOCATION=${BUILDDIR} \
-      -DOPENEXR_ROOT_DIR=${BUILDDIR} \
-      -DOPENSUBDIV_ROOT_DIR=${BUILDDIR} \
-      -DQT_USE_FRAMEWORKS=0 \
-      -DQT_ROOT_DIR=${BUILDDIR} \
-      -DPTEX_LOCATION=${BUILDDIR} \
-      -DTBB_ROOT_DIR=${BUILDDIR} \
-      -DBoost_INCLUDE_DIR=${BUILDDIR}/include -DBoost_LIBRARY_DIR=${BUILDDIR}/lib
+    exit 
   fi
+  echo "Configuring for make"
+  cmake ${SOURCEDIR} \
+    -DCMAKE_INSTALL_PREFIX=${BUILDDIR} \
+    -DCMAKE_PREFIX_PATH=${BUILDDIR} \
+    -DALEMBIC_DIR=${BUILDDIR} \
+    -DDOUBLE_CONVERSION_DIR=${BUILDDIR} \
+    -DGLEW_LOCATION=${BUILDDIR} \
+    -DOIIO_LOCATION=${BUILDDIR} \
+    -DOPENEXR_ROOT_DIR=${BUILDDIR} \
+    -DOPENSUBDIV_ROOT_DIR=${BUILDDIR} \
+    -DQT_USE_FRAMEWORKS=0 \
+    -DQT_ROOT_DIR=${BUILDDIR} \
+    -DPTEX_LOCATION=${BUILDDIR} \
+    -DTBB_ROOT_DIR=${BUILDDIR} \
+    -DBoost_INCLUDE_DIR=${BUILDDIR}/include -DBoost_LIBRARY_DIR=${BUILDDIR}/lib
 fi
