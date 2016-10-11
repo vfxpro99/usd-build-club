c12634f03e41ec6d7d1bacdbe0c80c3d49c2933e
VfxPro99 <vfxpro99@gmail.com>
2016-10-10 18:03:16 -0700

    More bash comparisons

--- build_prerequisites.sh
+++ build_prerequisites.sh
@@ -5,7 +5,7 @@ PREREQ_SCRIPTDIR=`dirname $0`
 ROOT=$(pwd)
 LOCAL=${ROOT}/local
 
-if [ $# > 1 ]; then
+if [ $# -ge 1 ]; then
   LOCAL=$1
 fi
 
@@ -18,21 +18,21 @@ source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/boost.sh $LOCAL
 
 # $? is the result of the most recent command
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
 echo --- double-conversion ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/double-conversion.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
 echo --- glew ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/glew.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
@@ -42,14 +42,14 @@ fi
 echo --- OpenColorIO.sh ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenColorIO.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo OpenColorIO build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- OpenEXR ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenEXR.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo OpenEXR build did not finish with a clean exit code, stopping
   exit $rc
 fi
@@ -57,63 +57,63 @@ fi
 echo --- OpenSubdiv ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenSubdiv.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
 echo --- jpeg ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/jpeg.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo jpeg build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- png ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/png.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo png build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- ptex ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/ptex.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
 echo --- tiff ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/tiff.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo tiff build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- tbb ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/tbb.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
 echo --- hdf5 ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/hdf5.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo HDF5 build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- alembic ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/alembic.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo Alembic build did not finish with a clean exit code, continuing nonetheless
 fi
 
 echo --- OpenImageIO ---
 source ${PREREQ_SCRIPTDIR}/prerequisites-macOS/OpenImageIO.sh $LOCAL
 rc=$?
-if [ $rc != 0 ]; then
+if [ $rc -ne 0 ]; then
   echo OpenImageIO build did not finish with a clean exit code, continuing nonetheless
 fi
 
