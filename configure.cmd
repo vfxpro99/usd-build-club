f851f7fbde8dc07b59217d63fea283c671f4b8c3
vfxpro99 <vfxpro99@gmail.com>
2016-10-23 18:54:38 -0700

    Do not validate schema generation on Windows

--- configure.cmd
+++ configure.cmd
@@ -10,6 +10,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
 REM // USE_PTEX=0 because ptex integration is currently broken with the latest version of ptex
 cmake ..\..\..\..\USD ^
+      -DPXR_VALIDATE_GENERATED_CODE=OFF ^
       -DPXR_BUILD_MAYA_PLUGIN=0 ^
       -DPXR_BUILD_KATANA_PLUGIN=0 ^
       -DPXR_BUILD_ALEMBIC_PLUGIN=0 ^
