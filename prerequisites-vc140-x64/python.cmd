c294016ef32c5fe23b1dd0cac498353b485b3876
vfxpro99 <vfxpro99@gmail.com>
2016-12-04 18:43:31 -0800

    Update python cmd to install jinja2

--- prerequisites-vc140-x64/python.cmd
+++ prerequisites-vc140-x64/python.cmd
@@ -2,3 +2,5 @@ ECHO setting up Python
 echo Warning, this script hacks pyconfig.h to disable the autolinking of python_d.lib
 set SCRIPT_DIR=%~dp0
 xcopy "%SCRIPT_DIR%pyconfig.h" "c:\Python27\include\pyconfig.h" /s /y
+
+pip install jinja2
