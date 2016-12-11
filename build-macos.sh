a38487025cd23aca6b90bf62df49869851a0d6a2
vfxpro99 <vfxpro99@gmail.com>
2016-12-10 22:39:40 -0800

    macos - use brew to install Qt, then install PySide

--- build-macos.sh
+++ build-macos.sh
@@ -7,28 +7,47 @@ ROOT=$(pwd)
 BREW=$ROOT/homebrew/bin/brew
 
 echo "-------------------------------------------------"
-echo "1/6 Checking Cmake"
+echo "1/7 Checking Cmake"
 echo "-------------------------------------------------"
 
 if [ ! -f "$BREW" ]; then
   source $PREREQ_SCRIPTDIR/brew/brew-install.sh
 fi
 
+PATH=${PATH}:$ROOT/homebrew/bin
+
 $BREW update
 $BREW upgrade
 $BREW install cmake
 
-PATH=${PATH}:$ROOT/homebrew/bin
-
 if hash cmake 2>/dev/null; then
   echo "Cmake up to date"
 else
-  echo "cmake not detected, exitting."
+  echo "cmake not detected, exiting."
   exit 1
 fi
 
 echo "-------------------------------------------------"
-echo "2/6 Building Prerequsities for USD"
+echo "2/7 Installing Qt and PySide for usdview"
+echo "-------------------------------------------------"
+
+$BREW install cartr/qt4/qt  
+
+if hash qmake 2>/dev/null; then
+  echo "Qt up to date"
+else
+  echo "qmake not detected, usdview will not be available."
+fi
+
+if [ ! -f $ROOT/local/bin/pyside-uic ]; then
+  echo "Installing PySide. This takes several minutes."
+  pip install --install-option="--prefix=$ROOT/local/" -U PySide
+fi
+
+echo "PySide up to date"
+
+echo "-------------------------------------------------"
+echo "3/7 Building Prerequsities for USD"
 echo "-------------------------------------------------"
 
 $PREREQ_SCRIPTDIR/build-prerequisites-macos.sh $ROOT/local
@@ -40,7 +59,7 @@ if [ $rc -ne 0 ]; then
 fi
 
 echo "-------------------------------------------------"
-echo "3/6 Getting the latest USD dev code"
+echo "4/7 Getting the latest USD dev code"
 echo "-------------------------------------------------"
 
 if [ ! -f ../USD/.git/config ]; then
@@ -54,7 +73,7 @@ git pull
 cd "$ROOT"
 
 echo "-------------------------------------------------"
-echo "4/6 Configuring the Xcode build for USD"
+echo "5/7 Configuring the Xcode build for USD"
 echo "-------------------------------------------------"
 
 source ${PREREQ_SCRIPTDIR}/configure-macos.sh Xcode
@@ -65,7 +84,7 @@ if [ $rc -ne 0 ]; then
 fi
 
 echo "-------------------------------------------------"
-echo "5/6 Building and installing USD"
+echo "6/7 Building and installing USD"
 if [[ "$1" = "Debug" ]]; then
   echo "  Debug build in progress."
   cmake --build . --target install --config Debug
@@ -82,7 +101,7 @@ if [ $rc -ne 0 ]; then
 fi
 
 echo "-------------------------------------------------"
-echo "6/6 Finalizing the build"
+echo "7/7 Finalizing the build"
 echo "-------------------------------------------------"
 
 # done
